40c7a9ced164b72406e3dd73c92dc0e519bf2f4a
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-10-08 20:02:32 -0300

    Commit Inicial

---
+++ resources/js/router/routes.js
@@ -0,0 +1,12 @@
+import Home from '../pages/Home.vue';
+
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home,
+    }
+]
+
+
+export default routes;
---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Nutrinatalia</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <app></app>
+        </div>
+
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
