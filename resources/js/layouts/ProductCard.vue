b2f0000ac1d33308800dabff2f446bb0c19a103b
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-10-16 17:58:15 -0300

    Agregamos los parametros: poster, title y description al componente productCard

--- resources/js/layouts/ProductCard.vue
+++ resources/js/layouts/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="product-card">
     <v-card elevation="5" class="mx-auto rounded-lg">
-      <v-img :src="this.type.posterSrc"></v-img>
+      <v-img :src="this.poster"></v-img>
       <v-card-title
         class="
           primary-font
@@ -10,16 +10,13 @@
           justify-center
           pb-2
         "
-        ><h3 class="product-card-title">{{ this.type.name }}</h3>
+        ><h3 class="product-card-title">{{ this.title }}</h3>
       </v-card-title>
       <v-card-text class="secondary-font pb-4">
-        <p class="mb-0 font-weight-bold">
-          Son videos de aproximadamente 2 horas con el paso a paso de recetas
-          saludables, deliciosas y fáciles de ejecutar.
-        </p>
+        <p class="mb-0">{{ this.description }}</p>
       </v-card-text>
       <div class="mx-4 pb-4">
-        <v-btn color="primary" block>Ver todos los {{ this.type.name }}</v-btn>
+        <v-btn color="primary" block>Ver todos los {{ this.title }}</v-btn>
       </div>
     </v-card>
   </article>
@@ -28,7 +25,7 @@
 <script>
 import { mapGetters } from 'vuex';
 export default {
-    props: ['type'],
+    props: ['poster', 'title', 'description'],
     computed: {
         ...mapGetters('productCard', ['getProductsTypes', 'getProductTypeText'])
     }
