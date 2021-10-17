1038f500856aec92d3c7318ed9a7930b90d90cc5
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-10-17 19:38:36 -0300

    Agregamos un testimonio estatico de prueba en el home

--- resources/js/components/home/SectionTestimonials.vue
+++ resources/js/components/home/SectionTestimonials.vue
@@ -3,13 +3,25 @@
     <h2 class="primary-font font-weight-bold mb-3 grey--text text--lighten-5">
       Testimonios
     </h2>
-    <p class="grey--text text--lighten-5">
+    <p class="grey--text text--lighten-5 mb-0">
       Comparto <span class="grey--text text--darken-4">contigo</span> los
       <span class="grey--text text--darken-4">comentarios</span> de mis alumnos.
     </p>
+    <v-container>
+      <v-row class="flex-nowrap overflow-x-auto">
+        <v-col cols="12">
+          <testimony></testimony>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
 
 <script>
-export default {};
+import testimony from './Testimony.vue';
+export default {
+  components: {
+    testimony
+  }
+};
 </script>
