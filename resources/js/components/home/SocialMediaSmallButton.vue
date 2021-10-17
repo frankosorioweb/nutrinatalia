2cd3cfe2aa7cb84faec7ec6f10744c491e2c4bf6
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-10-17 19:35:21 -0300

    Definimos la estructura y los estilos del SocialMediaSmallButton

--- resources/js/components/home/SocialMediaSmallButton.vue
+++ resources/js/components/home/SocialMediaSmallButton.vue
@@ -1,5 +1,20 @@
-<template></template>
+<template>
+  <a
+    class="d-inline-flex justify-center align-center social-media-small-button rounded-circle pa-1"
+    :class="{'facebook': type, 'instagram': type === false}"
+    :href="link"
+    target="_blank"
+  >
+    <v-icon class="icon">mdi-{{ type ? 'facebook' : 'instagram' }}</v-icon>
+  </a>
+</template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  props: {
+    type: Boolean,
+    link: String
+  }
+};
 </script>
