0b90b07d9a1fec68ca036b6094979a72a6fb1f2e
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-10-21 10:39:39 -0300

    Agregamos un id y un poster como campos en products.js

--- resources/js/store/data/products.js
+++ resources/js/store/data/products.js
@@ -1,14 +1,19 @@
 import prices from './prices';
 import productTypes from '../modules/productTypes';
 
+const postersSrc = '/img/posters/';
 const { WORKSHOP, EBOOK } = productTypes.state.types;
 
 // Aquí definimos los precios de los WORKSHOPS e EBOOKS
-const products = [{
-    name: 'Dulces que nos hacen bien',
-    description: 'Aprende a ejecutar recetas de postres dulces saludables, sin azúcar y sin harinas, con pocos pasos, ingredientes sencillos y súper deliciosos.',
-    price: prices[WORKSHOP].off60,
-    type: WORKSHOP
-}];
+const products = [
+    {
+        id: 1,
+        poster: `${postersSrc}Panes sin culpa.png`,
+        name: 'Dulces que nos hacen bien',
+        description: 'Aprende a ejecutar recetas de postres dulces saludables, sin azúcar y sin harinas, con pocos pasos, ingredientes sencillos y súper deliciosos.',
+        price: prices[WORKSHOP].off60,
+        type: WORKSHOP
+    }
+];
 
 export default products;
