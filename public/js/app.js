559d17445875797d6b8bdaf0eff9118fb7076f04
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-10-22 13:11:24 -0300

    Creamos la seccion FAQ

--- public/js/app.js
+++ public/js/app.js
@@ -2106,6 +2106,23 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionFAQ.vue?vue&type=script&lang=js&":
+/*!**********************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionFAQ.vue?vue&type=script&lang=js& ***!
+  \**********************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionGift.vue?vue&type=script&lang=js&":
 /*!***********************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionGift.vue?vue&type=script&lang=js& ***!
@@ -2846,6 +2863,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_home_SectionAboutAcademy_vue__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/home/SectionAboutAcademy.vue */ "./resources/js/components/home/SectionAboutAcademy.vue");
 /* harmony import */ var _components_home_SectionTestimonials_vue__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../components/home/SectionTestimonials.vue */ "./resources/js/components/home/SectionTestimonials.vue");
 /* harmony import */ var _components_home_SectionGift_vue__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../components/home/SectionGift.vue */ "./resources/js/components/home/SectionGift.vue");
+/* harmony import */ var _components_home_SectionFAQ_vue__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../components/home/SectionFAQ.vue */ "./resources/js/components/home/SectionFAQ.vue");
 //
 //
 //
@@ -2861,6 +2879,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+
 
 
 
@@ -2878,7 +2898,8 @@ __webpack_require__.r(__webpack_exports__);
     SectionPresentationVideo: _components_home_SectionPresentationVideo_vue__WEBPACK_IMPORTED_MODULE_4__["default"],
     SectionAboutAcademy: _components_home_SectionAboutAcademy_vue__WEBPACK_IMPORTED_MODULE_5__["default"],
     sectionTestimonials: _components_home_SectionTestimonials_vue__WEBPACK_IMPORTED_MODULE_6__["default"],
-    sectionGift: _components_home_SectionGift_vue__WEBPACK_IMPORTED_MODULE_7__["default"]
+    sectionGift: _components_home_SectionGift_vue__WEBPACK_IMPORTED_MODULE_7__["default"],
+    sectionFAQ: _components_home_SectionFAQ_vue__WEBPACK_IMPORTED_MODULE_8__["default"]
   }
 });
 
@@ -39828,6 +39849,22 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/js/components/home/SectionFAQ.vue?vue&type=script&lang=js&":
+/*!******************************************************************************!*\
+  !*** ./resources/js/components/home/SectionFAQ.vue?vue&type=script&lang=js& ***!
+  \******************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionFAQ_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./SectionFAQ.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionFAQ.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionFAQ_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
 /***/ "./resources/js/components/home/SectionGift.vue?vue&type=script&lang=js&":
 /*!*******************************************************************************!*\
   !*** ./resources/js/components/home/SectionGift.vue?vue&type=script&lang=js& ***!
@@ -40082,6 +40119,23 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionAboutAcademy_vue_vue_type_template_id_1148f21f___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
 /* harmony export */ });
 /* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionAboutAcademy_vue_vue_type_template_id_1148f21f___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./SectionAboutAcademy.vue?vue&type=template&id=1148f21f& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionAboutAcademy.vue?vue&type=template&id=1148f21f&");
+
+
+/***/ }),
+
+/***/ "./resources/js/components/home/SectionFAQ.vue?vue&type=template&id=5b4e2574&":
+/*!************************************************************************************!*\
+  !*** ./resources/js/components/home/SectionFAQ.vue?vue&type=template&id=5b4e2574& ***!
+  \************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionFAQ_vue_vue_type_template_id_5b4e2574___WEBPACK_IMPORTED_MODULE_0__.render),
+/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionFAQ_vue_vue_type_template_id_5b4e2574___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
+/* harmony export */ });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_SectionFAQ_vue_vue_type_template_id_5b4e2574___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./SectionFAQ.vue?vue&type=template&id=5b4e2574& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionFAQ.vue?vue&type=template&id=5b4e2574&");
 
 
 /***/ }),
@@ -40436,6 +40490,31 @@ var staticRenderFns = [
     ])
   }
 ]
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionFAQ.vue?vue&type=template&id=5b4e2574&":
+/*!***************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/home/SectionFAQ.vue?vue&type=template&id=5b4e2574& ***!
+  \***************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* binding */ render),
+/* harmony export */   "staticRenderFns": () => (/* binding */ staticRenderFns)
+/* harmony export */ });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div")
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -41614,7 +41693,9 @@ var render = function() {
           _vm._v(" "),
           _c("section-testimonials"),
           _vm._v(" "),
-          _c("section-gift")
+          _c("section-gift"),
+          _vm._v(" "),
+          _c("section-FAQ")
         ],
         1
       )
@@ -57823,6 +57904,45 @@ _node_modules_vuetify_loader_lib_runtime_installComponents_js__WEBPACK_IMPORTED_
 /* hot reload */
 if (false) { var api; }
 component.options.__file = "resources/js/components/home/SectionAboutAcademy.vue"
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/home/SectionFAQ.vue":
+/*!*****************************************************!*\
+  !*** ./resources/js/components/home/SectionFAQ.vue ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _SectionFAQ_vue_vue_type_template_id_5b4e2574___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./SectionFAQ.vue?vue&type=template&id=5b4e2574& */ "./resources/js/components/home/SectionFAQ.vue?vue&type=template&id=5b4e2574&");
+/* harmony import */ var _SectionFAQ_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./SectionFAQ.vue?vue&type=script&lang=js& */ "./resources/js/components/home/SectionFAQ.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+;
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _SectionFAQ_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _SectionFAQ_vue_vue_type_template_id_5b4e2574___WEBPACK_IMPORTED_MODULE_0__.render,
+  _SectionFAQ_vue_vue_type_template_id_5b4e2574___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/home/SectionFAQ.vue"
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
 
 /***/ }),
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,37 +1,40 @@
 <template>
-    <div>
-        <app-header></app-header>
-        <app-navigation-drawer></app-navigation-drawer>
-        <v-main>
-            <section-presentation></section-presentation>
-            <section-social-media></section-social-media>
-            <section-presentation-video></section-presentation-video>
-            <section-about-academy></section-about-academy>
-            <section-testimonials></section-testimonials>
-            <section-gift></section-gift>
-        </v-main>
-    </div>
+  <div>
+    <app-header></app-header>
+    <app-navigation-drawer></app-navigation-drawer>
+    <v-main>
+      <section-presentation></section-presentation>
+      <section-social-media></section-social-media>
+      <section-presentation-video></section-presentation-video>
+      <section-about-academy></section-about-academy>
+      <section-testimonials></section-testimonials>
+      <section-gift></section-gift>
+      <section-FAQ />
+    </v-main>
+  </div>
 </template>
 
 <script>
-import AppHeader from '../layouts/Header.vue'
-import AppNavigationDrawer from '../layouts/NavigationDrawer.vue'
-import SectionPresentation from '../components/home/SectionPresentation.vue'
-import SectionSocialMedia from '../components/home/SectionSocialMedia.vue';
-import SectionPresentationVideo from '../components/home/SectionPresentationVideo.vue';
-import SectionAboutAcademy from '../components/home/SectionAboutAcademy.vue'
-import sectionTestimonials from '../components/home/SectionTestimonials.vue';
-import sectionGift from '../components/home/SectionGift.vue';
+import AppHeader from "../layouts/Header.vue";
+import AppNavigationDrawer from "../layouts/NavigationDrawer.vue";
+import SectionPresentation from "../components/home/SectionPresentation.vue";
+import SectionSocialMedia from "../components/home/SectionSocialMedia.vue";
+import SectionPresentationVideo from "../components/home/SectionPresentationVideo.vue";
+import SectionAboutAcademy from "../components/home/SectionAboutAcademy.vue";
+import sectionTestimonials from "../components/home/SectionTestimonials.vue";
+import sectionGift from "../components/home/SectionGift.vue";
+import sectionFAQ from "../components/home/SectionFAQ.vue";
 export default {
-    components: {
-        AppHeader,
-        AppNavigationDrawer,
-        SectionPresentation,
-        SectionSocialMedia,
-        SectionPresentationVideo,
-        SectionAboutAcademy,
-        sectionTestimonials,
-        sectionGift
-    }
-}
+  components: {
+    AppHeader,
+    AppNavigationDrawer,
+    SectionPresentation,
+    SectionSocialMedia,
+    SectionPresentationVideo,
+    SectionAboutAcademy,
+    sectionTestimonials,
+    sectionGift,
+    sectionFAQ,
+  },
+};
 </script>
