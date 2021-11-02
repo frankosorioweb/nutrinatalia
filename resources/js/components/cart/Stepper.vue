75f5237ba76a35bf367f89c36406a5e3a2e53038
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-11-02 11:23:46 -0300

    Agregamos mayor margen vertical al titulo principal del cart

--- resources/js/components/cart/Stepper.vue
+++ resources/js/components/cart/Stepper.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="stepper">
-    <v-stepper>
+    <v-stepper class="rounded-lg">
       <v-stepper-header>
         <v-stepper-step step="1"> Ad unit details </v-stepper-step>
 
@@ -13,6 +13,6 @@
         <v-stepper-step step="3"> Ad sizes </v-stepper-step>
       </v-stepper-header>
     </v-stepper>
-    <h1 class="main-title primary-font text-center lh-1 my-3">Elegi tu método de pago</h1>
+    <h1 class="main-title primary-font text-center lh-1 my-6">Elegi tu método de pago</h1>
   </div>
 </template>
