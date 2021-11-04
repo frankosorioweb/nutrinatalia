ea5045882ba8d7912b8b81be9373845d713ad629
Francisco Osorio <osoriofranciscoj99@gmail.com>
2021-11-04 11:42:13 -0300

    Removemos los detalles del stepper para que solo muestre los pasos en numeros

--- resources/js/components/cart/Stepper.vue
+++ resources/js/components/cart/Stepper.vue
@@ -2,15 +2,15 @@
   <div class="stepper">
     <v-stepper class="rounded-lg">
       <v-stepper-header>
-        <v-stepper-step step="1"> Ad unit details </v-stepper-step>
+        <v-stepper-step step="1"></v-stepper-step>
 
         <v-divider></v-divider>
 
-        <v-stepper-step step="2"> Ad templates </v-stepper-step>
+        <v-stepper-step step="2"></v-stepper-step>
 
         <v-divider></v-divider>
 
-        <v-stepper-step step="3"> Ad sizes </v-stepper-step>
+        <v-stepper-step step="3"></v-stepper-step>
       </v-stepper-header>
     </v-stepper>
     <h1 class="main-title primary-font text-center lh-1 my-6">Elegi tu método de pago</h1>
