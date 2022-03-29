91ff306861ae29e9007b0f0750ae222bf8c09664
Francisco Osorio <osoriofranciscoj99@gmail.com>
2022-03-29 15:32:05 -0400

    Agregamos los datos reales de los testimonios

--- resources/js/store/data/testimonials.js
+++ resources/js/store/data/testimonials.js
@@ -1,41 +1,30 @@
 const avatarSource = '/img/avatars';
 const testimonials = [
     {
-        avatar: `${avatarSource}/1.png`,
-        name: 'Will Smith',
-        text: 'La mejor academia online de nutrición del planeta, mi calificación son cinco estrellas para la plataforma.',
+        avatar: `${avatarSource}/1 - Leticia Villagra.png`,
+        name: 'Leticia Villagra',
+        text: 'Aprender con consciencia sobre tus propios errores, esas son las herramientas que encontré con la Nutri Natalia,y lo llevo para toda la vida.',
         socialMedia: {
-            facebook: 'https://facebook.com',
-            instagram: 'https://instagram.com'
+            facebook: 'https://www.facebook.com/lety.villagra',
+            instagram: 'https://instagram.com/lettuvillagra'
         }
     },
     {
-        avatar: `${avatarSource}/1.png`,
-        name: 'Will Smith',
-        text: 'La mejor academia online de nutrición del planeta, mi calificación son cinco estrellas para la plataforma.',
+        avatar: `${avatarSource}/2 - Araceli Esquivel.png`,
+        name: 'Araceli Esquivel',
+        text: 'Soy empredora de dulces saludables, conocí a la Nutri Natalia en su taller de panes sin culpas, inspirada en mis clientes, desde ahi mi interes en este estilo de vida. Consultar con la Nutri fue la mejor inversión que hice por mi, mi vida cambió muchísimo ya no me siento cansada e hinchada, lo disfruto, llevo 12 meses como estilo de vida y amo.',
         socialMedia: {
-            facebook: 'https://facebook.com',
-            instagram: 'https://instagram.com'
+            instagram: 'https://instagram.com/araceliesquivelc'
         }
     },
     {
-        avatar: `${avatarSource}/1.png`,
-        name: 'Will Smith',
-        text: 'La mejor academia online de nutrición del planeta, mi calificación son cinco estrellas para la plataforma.',
+        avatar: `${avatarSource}/3 - Raquel Oviedo.png`,
+        name: 'Raquel Oviedo',
+        text: 'El ebook Plan Keto + Ayuno me empoderó a priorizar mi salud y bienestar. No tiene secretos, no es una dieta, simplemente te guía a un estilo de vida en base a los principios de la naturaleza. Aprendí que "La información es poder".',
         socialMedia: {
-            facebook: 'https://facebook.com',
-            instagram: 'https://instagram.com'
+            instagram: 'https://instagram.com/oviedoraquel'
         }
-    },
-    {
-        avatar: `${avatarSource}/1.png`,
-        name: 'Will Smith',
-        text: 'La mejor academia online de nutrición del planeta, mi calificación son cinco estrellas para la plataforma.',
-        socialMedia: {
-            facebook: 'https://facebook.com',
-            instagram: 'https://instagram.com'
-        }
-    },
+    }
 ];
 
 export default testimonials;
