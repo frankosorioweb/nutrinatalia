ca5a3ae9cc92115b3e5054490eecb6f9e062fea9
Francisco Osorio <osoriofranciscoj99@gmail.com>
2022-03-31 16:06:36 -0400

    Agregamos algunos metadatos importantes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Alcanza tus metas de salud y peso mediante los recursos alojados en esta academia, soy Natalia Chaparro Fox, licenciada en nutrición y ayudo a las personas a mejorar su vida en el ámbito de la salud.">
+        <meta property="og:description" content="Alcanza tus metas de salud y peso mediante los recursos alojados en esta academia, soy Natalia Chaparro Fox, licenciada en nutrición y ayudo a las personas a mejorar su vida en el ámbito de la salud.">
 
-        <title>Nutrinatalia</title>
+        <title>Nutrinatalia | La academia</title>
+        <meta property='og:title'  content='Nutrinatalia | La academia'>
+
+        <link rel='icon' type='image/ico' href='{{ asset('favicon.ico') }}'>
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
