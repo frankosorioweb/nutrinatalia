c100bce138a006920d11030132aa1eb188a2ab9e
Francisco Osorio <osoriofranciscoj99@gmail.com>
2022-04-09 17:35:08 -0400

    Agregamos el fragmento de google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,37 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Alcanza tus metas de salud y peso mediante los recursos alojados en esta academia, soy Natalia Chaparro Fox, licenciada en nutrición y ayudo a las personas a mejorar su vida en el ámbito de la salud.">
-        <meta property="og:description" content="Alcanza tus metas de salud y peso mediante los recursos alojados en esta academia, soy Natalia Chaparro Fox, licenciada en nutrición y ayudo a las personas a mejorar su vida en el ámbito de la salud.">
 
-        <title>Nutrinatalia | La academia</title>
-        <meta property='og:title'  content='Nutrinatalia | La academia'>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Alcanza tus metas de salud y peso mediante los recursos alojados en esta academia, soy Natalia Chaparro Fox, licenciada en nutrición y ayudo a las personas a mejorar su vida en el ámbito de la salud.">
+    <meta property="og:description" content="Alcanza tus metas de salud y peso mediante los recursos alojados en esta academia, soy Natalia Chaparro Fox, licenciada en nutrición y ayudo a las personas a mejorar su vida en el ámbito de la salud.">
 
-        <link rel='icon' type='image/ico' href="{{ asset('favicon.ico') }}">
+    <title>Nutrinatalia | La academia</title>
+    <meta property='og:title' content='Nutrinatalia | La academia'>
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    </head>
-    <body>
-        <div id="app">
-            <app></app>
-        </div>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
+    <!-- GOOGLE ANALYTICS -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2DGT6KSRBC"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-2DGT6KSRBC');
+    </script>
+</head>
+
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
 </html>
